3464e055e33aa324e3b8cdaab0f5d80547cc70d5
emericgautier <emericgtr@gmail.com>
2022-09-23 13:02:21 +0200

    increment in store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Form from "./components/Form";
 import PicCard from "./components/PicCard";
+import { setPicturesData } from "./feature/pictures.slice";
 
 const App = () => {
-  const [picsData, setPicsData] = useState([]);
+  const dispatch = useDispatch();
+  const picsData = useSelector((state) => state.pictures.pictures);
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/pictures")
-      .then((res) => setPicsData(res.data));
+      .then((res) => dispatch(setPicturesData(res.data)));
   }, []);
 
   return (
@@ -17,7 +20,7 @@ const App = () => {
       <h1>NFT Gallery</h1>
       <Form />
       <div className="cards-container">
-        {picsData.map((pic, index) => (
+        {picsData?.map((pic, index) => (
           <PicCard key={index} pic={pic} />
         ))}
       </div>
